56b5e51765074b8931edbc60491b677a5bdc6e1c
SaoirseODonovan <74667820+SaoirseODonovan@users.noreply.github.com>
2022-10-26 21:57:47 +0100

    Fixed Favourites Page Showing Movies.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import {createRoot} from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
-import favouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
+import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
 import {Link} from 'react-router-dom';
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader';
@@ -11,6 +11,7 @@ import UpcomingMoviesPage from "./pages/upcomingMoviesPage.js";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage';
 
 //Immediately after the import statements, declare the query client (it will manage the cache in the browser)
 //The above configuration will retain all data in the cache for 1 hour before it becomes invalidated.
@@ -32,11 +33,12 @@ const App = () => {
       <MoviesContextProvider>
       <Routes>
       <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
-        <Route exact path="/movies/favourites" element={<favouriteMoviesPage />} />
+        <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={ <Navigate to="/" /> } />
         <Route exact path="/movies/upcoming" element={<UpcomingMoviesPage />} />
+        <Route path="/reviews/form" element={<AddMovieReviewPage/>} />
       </Routes>
       </MoviesContextProvider>
     </BrowserRouter>
