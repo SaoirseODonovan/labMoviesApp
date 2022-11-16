21cb8169187256a3585c268f1d63e64ffa94a547
SaoirseODonovan <74667820+SaoirseODonovan@users.noreply.github.com>
2022-11-16 18:19:48 +0000

    trying to fix similar movies
    no luck, error is Cannot read properties of undefined (reading 'id')

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -28,7 +28,7 @@ export const getMovie = (args) => {
     throw error
  });
 };
-  
+
   export const getGenres = async () => {
     return fetch(
       "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
@@ -129,7 +129,36 @@ export const getTrendingMovies = (args) => {
   });
 };
 
-// export const getSimilarMovies = (id) => {
+// export const getSimilarMovies = ({ queryKey }) => {
+//   const [, idPart] = queryKey;
+//   const { id } = idPart;
+//   return fetch(
+//     `https://api.themoviedb.org/3/movie/${id}/similar?api_key=api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+//   ).then( (response) => {
+//     if (!response.ok) {
+//       throw new Error(response.json().message);
+//     }
+//     return response.json();
+
+//   })
+//   .catch((error) => {
+//     throw error
+//  });
+// };
+
+// export const getSimilarMovies= (id) => {
+//   return fetch(
+//     `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+//   )
+//     .then((res) => res.json())
+//     .then((json) => {
+//       console.log(json.results);
+//       return json.results;
+//     });
+// };
+
+
+// export const getSimilarMovies = (args) => {
 //   return fetch(
 //     `https://api.themoviedb.org/3/movie/${id}/similar?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
 //   ).then((response) => {
@@ -143,19 +172,23 @@ export const getTrendingMovies = (args) => {
 //   });
 // };
 
-export const getSimilarMovies = (args) => {
+export const getSimilarMovies = ({ queryKey }) => {
+  const [, idPart] = queryKey;
+  const { id } = idPart;
   return fetch(
-    `https://api.themoviedb.org/3/movie/{movie_id}/similar?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
-  ).then((response) => {
+    `https://api.themoviedb.org/3/movie/${id}/similar?api_key=api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
+  ).then( (response) => {
     if (!response.ok) {
       throw new Error(response.json().message);
     }
     return response.json();
+
   })
   .catch((error) => {
-     throw error
-  });
+    throw error
+ });
 };
+
 
 export const getMovieActors = () => {
   return fetch(
